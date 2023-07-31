3b3cbb3a93c2c4732b1a628c257b9457ceab1756
Yugeon CHUN <ychun@paul-f4Br9s3.clusters.42paris.fr>
2023-07-31 21:31:55 +0200

    corn give up

--- rt_final/src_bonus/raytracing/hit_corn_bonus.c
+++ rt_final/src_bonus/raytracing/hit_corn_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ychun <ychun@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 02:05:33 by ychun             #+#    #+#             */
-/*   Updated: 2023/07/31 14:54:10 by ychun            ###   ########.fr       */
+/*   Updated: 2023/07/31 21:24:40 by ychun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,14 @@ static void	set_face_normal(t_ray ray, t_hit_record *rec)
 		rec->normal = vmult(rec->normal, -1);
 }
 
-int	get_corn_normal(t_cylinder *obj, t_hit_record *rec, t_vec3 p, double r)
+int	get_corn_normal(t_cylinder *obj, t_hit_record *rec, double hit_height, double r, t_vec3 p)
 {
-	double	hit_height;
 	double	max_height;
 	t_vec3	hit_normal;
 	t_vec3	hit_pos;
 
-	hit_height = vdot(vminus(p, obj->pos), obj->orivec);
 	max_height = obj->height;
-	if (hit_height > max_height)
+	if (hit_height >= max_height || hit_height < 2)
 		return (0);
 	hit_pos = vplus(obj->pos, vmult(obj->orivec, hit_height));
 	hit_normal = vunit(vminus(p, hit_pos));
@@ -38,14 +36,36 @@ int	get_corn_normal(t_cylinder *obj, t_hit_record *rec, t_vec3 p, double r)
 	return (1);
 }
 
+double	get_corn_hit_height(t_cylinder *obj, t_ray ray)
+{
+	t_hit_num	num;
+	double		ret;
+
+	num.p = vminus(ray.pos, obj->pos);
+	num.a = vlength2(vcross(ray.orivec, obj->orivec));
+	num.half_b = vdot(vcross(ray.orivec, obj->orivec),
+			vcross(num.p, obj->orivec));
+	num.c = vlength2(vcross(num.p, obj->orivec)) - pow(obj->diameter / 2, 2);
+	num.discriminant = num.half_b * num.half_b - num.a * num.c;
+	if (num.discriminant < 0)
+		return (-1);
+	num.root = (-num.half_b - sqrt(num.discriminant)) / num.a;
+	ret = vdot(vminus(vplus(ray.pos, vmult(ray.orivec, num.root)),
+				obj->pos), obj->orivec);
+	return (ret);
+}
+
 int	hit_corn_side(t_cylinder *obj, t_ray ray, t_hit_record *rec)
 {
 	t_hit_num	num;
 	double		r;
+	double		hit_height;
 
 	num.p = vminus(ray.pos, obj->pos);
-	r = ((obj->diameter / 2 * (obj->height - vdot(vminus(num.p, obj->pos),
-						obj->orivec))) / obj->height);
+	hit_height = get_corn_hit_height(obj, ray);
+	if (hit_height == -1)
+		return (0);
+	r = ((obj->diameter / 2) * (obj->height - hit_height)) / obj->height;
 	num.a = vlength2(vcross(ray.orivec, obj->orivec));
 	num.half_b = vdot(vcross(ray.orivec, obj->orivec),
 			vcross(num.p, obj->orivec));
@@ -60,7 +80,7 @@ int	hit_corn_side(t_cylinder *obj, t_ray ray, t_hit_record *rec)
 		if (num.root < rec->tmin || rec->tmax < num.root)
 			return (0);
 	}
-	if (!get_corn_normal(obj, rec, num.p, r))
+	if (!get_corn_normal(obj, rec, hit_height, r, vplus(ray.pos, vmult(ray.orivec, num.root))))
 		return (0);
 	rec->p = vplus(ray.pos, vmult(ray.orivec, num.root));
 	set_face_normal(ray, rec);
